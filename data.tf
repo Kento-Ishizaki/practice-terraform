366b0015f71ac056c03dc684d3b2f6a53fc6eed7
Kento-Ishizaki <kentoron1115@gmail.com>
2022-06-25 23:27:18 +0900

    fix: ami filter

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aws_ami" "app" {
   owners      = ["self", "amazon"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*.0-x86_64-gp2"]
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.*-x86_64-gp2"]
   }
   filter {
     name   = "root-device-type"
